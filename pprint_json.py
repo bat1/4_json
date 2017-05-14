ed89dd485bc6d43a785574cac9b55ffab34b6397
Maxim Batrakov <maxim.batrakov@gmail.com>
2017-05-14 20:05:00 +0000

    improved code in pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,18 @@
 import json
 import os 
 
-def load_data(file):
+def load_data(filepath):
+    if not os.path.exists(filepath):
+        return print("Вы ввели несуществующий путь к файлу. Введите правильный путь.")
+    with open(jsonfile, 'r') as file_handler:
+        return json.load(file_handler)
     
-    with open(file, 'r') as handle:
-        parsed = json.load(handle)
-        return parsed
-        
-def pretty_print_json(data):
-    print(json.dumps(data, ensure_ascii = False, indent=4, sort_keys=True))
+def pretty_print_json(raw_json):
+    print(json.dumps(raw_json, ensure_ascii = False, indent=4, sort_keys=True))
     
 if __name__ == '__main__':
 
-    file_path = os.path.abspath(input("Введите путь к файлу с JSON: ").strip()) # например /projects/4_json/jsonfile.txt
-    file = os.path.basename(file_path)
-    data = load_data(file_path)
-    pretty_print_json(data)
+    filepath = os.path.abspath(input("Введите путь к файлу с JSON: ").strip()) # например /projects/4_json/jsonfile.txt
+    jsonfile = os.path.basename(filepath)
+    raw_json = load_data(filepath)
+    pretty_print_json(raw_json)
