90433b40a034bbfd1f4bbbcd86944a9d7c09a2df
Maxim Batrakov <maxim.batrakov@gmail.com>
2017-05-15 18:11:56 +0000

    git ignore added, pprint_json changed

--- pprint_json.py
+++ pprint_json.py
@@ -3,12 +3,12 @@ import os
 
 def load_data(filepath):
     if not os.path.exists(filepath):
-        return print("Вы ввели несуществующий путь к файлу. Введите правильный путь.")
+        print("Вы ввели несуществующий путь к файлу. Введите правильный путь.")
     with open(jsonfile, 'r') as file_handler:
         return json.load(file_handler)
     
 def pretty_print_json(raw_json):
-    print(json.dumps(raw_json, ensure_ascii = False, indent=4, sort_keys=True))
+    return print(json.dumps(raw_json, ensure_ascii = False, indent=4, sort_keys=True))
     
 if __name__ == '__main__':
 
